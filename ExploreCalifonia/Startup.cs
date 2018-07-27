2c99f9d69078e9902cfe25d124bcee9542a5f338
Prem Rajah <prajah@ctlondon.com>
2018-07-27 12:08:31 +0100

    Start

--- ExploreCalifonia/Startup.cs
+++ ExploreCalifonia/Startup.cs
@@ -27,7 +27,7 @@ namespace ExploreCalifonia
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello ASP!");
             });
         }
     }
